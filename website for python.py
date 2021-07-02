7289888522550cd3e854d39daaeb3221e5bb1c8f
TarekMAshour <86852326+TarekMAshour@users.noreply.github.com>
2021-07-02 21:53:33 +0200

    Add files via upload

---
+++ website for python.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+window = tk.Tk
+website_greeting = tk.Label(text = 'Enter your username.')
+website_username = tk.Entry()
+
+website_user_storage = website_username.get()
+f = open("username saves", "a")
+f.write(website_user_storage)
+file.close()
+#how do I make the user make a username on time only
+
+website_greeting.pack()
+website_username.pack()
+tk.mainloop()
