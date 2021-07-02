d7d154d8c87d3e1e4ac9a1f0253aad55b0e16f53
TarekMAshour <86852326+TarekMAshour@users.noreply.github.com>
2021-07-02 21:57:50 +0200

    Update website for python.py

--- website for python.py
+++ website for python.py
@@ -7,7 +7,7 @@ website_user_storage = website_username.get()
 f = open("username saves", "a")
 f.write(website_user_storage)
 file.close()
-#how do I make the user make a username on time only
+#how do I make the user make a username one time only
 
 website_greeting.pack()
 website_username.pack()
